32f3f2dff3d0e3deb98bd94e0dd445b29645f465
Dudz Artiaga <diosdado.artiaga@infor.com>
2020-10-28 23:58:06 +0800

    working wireframe

---
+++ ADAGroup.EIG/Web/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ActivitiesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ ADAGroup.EIG/Web/Controllers/GroupsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public GroupsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
